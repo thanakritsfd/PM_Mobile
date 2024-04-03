5b50fb0e102eb4e7b2c7956e68df8ff9a510dffd
thanakrit <ketaauto55@gmail.com>
2024-04-03 20:15:34 +0700

    Edit Overlay

--- lib/views/webview_screen.dart
+++ lib/views/webview_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: use_super_parameters
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulWidget {
@@ -20,6 +23,9 @@ class _WebViewScreenState extends State<WebViewScreen> {
       ..loadRequest(Uri.parse('https://pmstation.org/pm/'));
     _controller = controller;
     _controller.clearCache(); // ล้างแคชใน WebView
+
+    // Hide system overlays
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   }
 
   @override
@@ -27,8 +33,23 @@ class _WebViewScreenState extends State<WebViewScreen> {
     return Scaffold(
       body: SafeArea(
         top: false,
-        child: WebViewWidget(controller: _controller),
+        child: Column(
+          children: [
+            Expanded(
+              child: WebViewWidget(controller: _controller),
+            ),
+            // Bottom padding to avoid overlaying the system status bar
+            SizedBox(height: MediaQuery.of(context).padding.bottom),
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // Re-enable system overlays when the state is disposed
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    super.dispose();
   }
 }
